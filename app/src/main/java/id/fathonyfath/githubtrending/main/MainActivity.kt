5addf05256a6540d6ae81e50032be216e9bb5aa6
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2019-09-30 01:45:52 +0700

    feat: add theme, menu, and layout for MainActivity

--- app/src/main/java/id/fathonyfath/githubtrending/main/MainActivity.kt
+++ app/src/main/java/id/fathonyfath/githubtrending/main/MainActivity.kt
@@ -2,13 +2,19 @@ package id.fathonyfath.githubtrending.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import id.fathonyfath.githubtrending.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.main_menu)
     }
 }
