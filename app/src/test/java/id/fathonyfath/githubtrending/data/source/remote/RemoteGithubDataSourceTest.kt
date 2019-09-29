00b762cb6811d57f06deaa578873036c732bc42b
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2019-09-29 19:00:52 +0700

    test: update remote data source test to mock caching dependency

--- app/src/test/java/id/fathonyfath/githubtrending/data/source/remote/RemoteGithubDataSourceTest.kt
+++ app/src/test/java/id/fathonyfath/githubtrending/data/source/remote/RemoteGithubDataSourceTest.kt
@@ -1,20 +1,37 @@
 package id.fathonyfath.githubtrending.data.source.remote
 
 import id.fathonyfath.githubtrending.BuildConfig
+import id.fathonyfath.githubtrending.data.cache.RepositoriesCache
 import id.fathonyfath.githubtrending.di.DaggerTestAppComponent
+import id.fathonyfath.githubtrending.scheduler.TrampolineSchedulerProvider
+import io.reactivex.Completable
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.SocketPolicy
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
 import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
 import javax.inject.Inject
 
+@RunWith(MockitoJUnitRunner::class)
 class RemoteGithubDataSourceTest {
 
     @Inject
+    lateinit var trendingApi: TrendingApi
+
+    @Mock
+    lateinit var repositoriesCache: RepositoriesCache
+
+    @Inject
+    lateinit var schedulerProvider: TrampolineSchedulerProvider
+
     lateinit var remoteGithubDataSource: RemoteGithubDataSource
 
     private val mockWebServer = MockWebServer()
@@ -28,6 +45,9 @@ class RemoteGithubDataSourceTest {
             .newAppComponent(mockWebServer.url("/"), BuildConfig.DEBUG)
             .inject(this)
 
+        remoteGithubDataSource =
+            RemoteGithubDataSource(trendingApi, repositoriesCache, schedulerProvider)
+
     }
 
     @After
@@ -37,6 +57,8 @@ class RemoteGithubDataSourceTest {
 
     @Test
     fun popularRepositories_positiveResponse() {
+        `when`(repositoriesCache.put(ArgumentMatchers.anyList())).thenReturn(Completable.complete())
+
         val responseBody = requireNotNull(readFileFromResources("./responses/repositories.json"))
         val response = MockResponse()
             .setResponseCode(HttpURLConnection.HTTP_OK)
@@ -48,6 +70,8 @@ class RemoteGithubDataSourceTest {
             .test()
             .assertNoErrors()
             .dispose()
+
+        verify(repositoriesCache, times(1)).put(ArgumentMatchers.anyList())
     }
 
     @Test
@@ -60,6 +84,7 @@ class RemoteGithubDataSourceTest {
         remoteGithubDataSource.popularRepositories()
             .test()
             .assertError(SocketTimeoutException::class.java)
+            .dispose()
     }
 
     private fun readFileFromResources(path: String): String? {
